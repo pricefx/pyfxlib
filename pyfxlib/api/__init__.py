337a14bc95666d0291f6865ec2b7955bd1bb0fee
valerie.mauduit <valerie.mauduit@pricefx.com>
2025-11-10 17:29:19 +0100

    PFUN-32162 Last fixes to make pricefx-python-engine able to use pyfxlib

--- pyfxlib/api/__init__.py
+++ pyfxlib/api/__init__.py
@@ -6,3 +6,7 @@ Pricefx Python Client can be used outside a Pricefx logic. One can use the class
 `domain.Instance.connect` to manually connect to a Pricefx instance and use the
 returned `domain.Instance` object methods to navigate the partition objects.
 """
+
+from . import domain
+
+__all__ = ["domain"]
