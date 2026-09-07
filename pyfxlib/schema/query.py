7c41d5a53fd14af4214361b0c22e22006384762b
Augustin Degas <augustin.degas@pricefx.com>
2026-09-07 17:03:43 +0200

    PFS-383: Move all query API from genfx into pyfxlib

--- pyfxlib/schema/query.py
+++ pyfxlib/schema/query.py
@@ -32,6 +32,64 @@ class Operator(StrEnum):
     OR = "or"
     NOT = "not"
 
+class OperationAgg(StrEnum):
+    """Operation aggregate class."""
+
+    SUM = "sum"
+    AVG = "avg"
+    MIN = "min"
+    MAX = "max"
+    COUNT_ALL = "countAll"
+    COUNT_NON_NULL = "countNonNull"
+    COUNT_DISTINCT_NON_NULL = "countDistinctNonNull"
+
+class Operation(StrEnum):
+    """Operation class."""
+
+    AND = "and"
+    OR = "or"
+    NOT = "not"
+    ABS = "abs"
+    EXP = "exp"
+    CASE_WHEN = "caseWhen"
+    COALESCE = "coalesce"
+    NULL_IF = "nullIf"
+    LESS_THAN = "lessThan"
+    LESS_OR_EQUAL = "lessOrEqual"
+    GREATER_THAN = "greaterThan"
+    GREATER_OR_EQUAL = "greaterOrEqual"
+    EQUAL = "equal"
+    EQUAL_NULL_AWARE = "equalNullAware"
+    NOT_EQUAL = "notEqual"
+    NOT_EQUAL_NULL_AWARE = "notEqualNullAware"
+    LIKE = "like"
+    ILIKE = "ilike"
+    NOT_LIKE = "notLike"
+    NOT_ILIKE = "notIlike"
+    IN = "in"
+    NOT_IN = "notIn"
+    IS_NULL = "isNull"
+    IS_NOT_NULL = "isNotNull"
+    IS_TRUE = "isTrue"
+    IS_FALSE = "isFalse"
+    CONCAT = "concat"
+    SUBSTRING = "substring"
+    UPPER = "upper"
+    LOWER = "lower"
+    TRIM = "trim"
+    LENGTH = "length"
+    LOCATE = "locate"
+    PLUS = "plus"
+    MINUS = "minus"
+    MULTIPLY = "multiply"
+    DIV = "div"
+    MOD = "mod"
+    CAST_AS_INTEGER = "castAsInteger"
+    CAST_AS_BOOLEAN = "castAsBoolean"
+    CAST_AS_DATE_ONLY = "castAsDateOnly"
+    CAST_AS_DATE_TIME = "castAsDateTime"
+    CAST_AS_REAL = "castAsReal"
+    CAST_AS_STRING = "castAsString"
 
 class FilterOperator(StrEnum):
     """Filter operators for search criteria."""
@@ -249,6 +307,13 @@ class ProductExtensionRows(BaseModel):
     product_extension_name: str
 
 
+class CompanyParametersRows(BaseModel):
+    """Company parameters rows table."""
+
+    kind: Literal["companyParameterRow"] = "companyParameterRow"
+    name: str
+
+
 class PADataSource(BaseModel):
     """Data source table."""
 
@@ -285,6 +350,258 @@ class PADataFeed(BaseModel):
 
 
 Table = Products | ProductExtensionRows | PADataSource | PADatamart | PADataFeed
+
+
+# All Selectables
+
+
+class SourceColumnReference(BaseModel):
+    """Source column class."""
+
+    kind: Literal["columnReference"] = "columnReference"
+    column: str
+    source: Literal["table"] = "table"
+
+
+class PreviousStageColumnReference(BaseModel):
+    """Previous stage column class."""
+
+    kind: Literal["columnReference"] = "columnReference"
+    column: str
+    source: Literal["previousStage"] = "previousStage"
+
+
+class LiteralValue(BaseModel):
+    """Literal class."""
+
+    kind: Literal["literal"] = "literal"
+    type: LiteralType
+    value: Any
+
+
+class PreviousStageFunction(BaseModel):
+    """Function class."""
+
+    kind: Literal["function"] = "function"
+    name: Operation
+    arguments: list["PreviousStageExpression"]
+
+
+PreviousStageExpression: TypeAlias = (
+        LiteralValue | PreviousStageColumnReference | PreviousStageFunction
+)
+
+
+class SourceFunction(BaseModel):
+    """Source Function class."""
+
+    kind: Literal["function"] = "function"
+    name: Operation
+    arguments: list["SourceExpression"]
+
+
+SourceExpression: TypeAlias = LiteralValue | SourceColumnReference | SourceFunction
+
+
+class SourceSelectable(BaseModel):
+    """Source Selectable class."""
+
+    kind: Literal["selectable"] = "selectable"
+    expression: SourceExpression
+    alias: str
+
+
+class PreviousStageSelectable(BaseModel):
+    """Selectable class."""
+
+    kind: Literal["selectable"] = "selectable"
+    expression: PreviousStageExpression
+    alias: str
+
+
+# Special case for aggregable expressions
+
+
+class PreviousStageFunctionAgg(BaseModel):
+    """Function agg class."""
+
+    kind: Literal["function"] = "function"
+    name: OperationAgg
+    arguments: list[PreviousStageExpression]
+
+
+ExpressionAgg: TypeAlias = (
+        LiteralValue
+        | PreviousStageColumnReference
+        | PreviousStageFunctionAgg
+        | PreviousStageFunction
+)
+
+
+class PreviousStageSelectableAgg(BaseModel):
+    """Selectable agg class."""
+
+    kind: Literal["selectable"] = "selectable"
+    expression: ExpressionAgg
+    alias: str
+
+
+# All the stage types
+
+
+class Source(BaseModel):
+    """Source class."""
+
+    kind: Literal["source"] = "source"
+    table: Table
+    columns: Optional[list[SourceSelectable]] = None
+    # Note: We disable this field because the LLM has trouble generating proper criteria
+    # instead we rely on explicity filter stage for now.
+    # criteria: Optional[SourceExpression] = None
+
+
+class JoinType(StrEnum):
+    """Join type class."""
+
+    INNER = "INNER"
+    LEFT_OUTER = "LEFT_OUTER"
+
+
+class JoinFunction(BaseModel):
+    """Function class for joins."""
+
+    kind: Literal["function"] = "function"
+    name: Operation
+    arguments: list["JoinExpression"]
+
+
+JoinExpression: TypeAlias = (
+        LiteralValue | PreviousStageColumnReference | SourceColumnReference | JoinFunction
+)
+
+
+class Join(BaseModel):
+    """Inner join class."""
+
+    kind: Literal["join"] = "join"
+    table: Table
+    type: JoinType
+    columns: list[SourceSelectable]
+    criteria: JoinExpression
+
+
+class AddColumns(BaseModel):
+    """Add columns class."""
+
+    kind: Literal["addColumns"] = "addColumns"
+    columns: list[PreviousStageSelectable]
+
+
+class RemoveColumns(BaseModel):
+    """Remove columns class."""
+
+    kind: Literal["removeColumns"] = "removeColumns"
+    columns: list[str]
+
+
+class RetainColumns(BaseModel):
+    """Retain columns class."""
+
+    kind: Literal["retainColumns"] = "retainColumns"
+    columns: list[str]
+
+
+class Filter(BaseModel):
+    """Filter class."""
+
+    kind: Literal["filter"] = "filter"
+    criteria: PreviousStageExpression
+
+
+class Aggregate(BaseModel):
+    """Aggregate class."""
+
+    kind: Literal["aggregate"] = "aggregate"
+    columns: list[PreviousStageSelectableAgg]
+    dimensions: list[PreviousStageExpression]
+
+
+class Distinct(BaseModel):
+    """Distinct class."""
+
+    kind: Literal["distinct"] = "distinct"
+
+
+class Take(BaseModel):
+    """Take class."""
+
+    kind: Literal["take"] = "take"
+    count: int
+
+
+class OrderDirection(StrEnum):
+    """Order direction class."""
+
+    ASC_NULLS_FIRST = "ASC_NULLS_FIRST"
+    ASC_NULLS_LAST = "ASC_NULLS_LAST"
+    DESC_NULLS_FIRST = "DESC_NULLS_FIRST"
+    DESC_NULLS_LAST = "DESC_NULLS_LAST"
+
+
+class Order(BaseModel):
+    """Order class."""
+
+    kind: Literal["order"] = "order"
+    expression: PreviousStageExpression
+    direction: OrderDirection
+
+
+class Sort(BaseModel):
+    """Sort class."""
+
+    kind: Literal["sort"] = "sort"
+    orders: list[Order]
+
+
+Stage: TypeAlias = (
+        Source
+        | Join
+        | AddColumns
+        | RemoveColumns
+        | RetainColumns
+        | Filter
+        | Aggregate
+        | Distinct
+        | Sort
+        | Take
+)
+
+###
+
+
+class Pipeline(BaseModel):
+    """Pipeline class."""
+
+    kind: Literal["pipeline"] = "pipeline"
+    stages: list[Stage]
+
+class QueryAnswer(BaseModel):
+    """Result of the query."""
+
+    query: Pipeline
+    meta: QueryAnswerMeta
+    rows: list[list[Any]]
+
+    def to_pandas(self) -> pd.DataFrame:
+        """Convert the result to a pandas DataFrame."""
+        types_map = {}
+        for col in self.meta.columns:
+            types_map[col.name] = col.type.to_pandas_type()
+
+        return pd.DataFrame(
+            self.rows,
+            columns=[col.name for col in self.meta.columns],
+        ).astype(types_map)
 
 
 class QueryAnswerMetaColumn(BaseModel):
